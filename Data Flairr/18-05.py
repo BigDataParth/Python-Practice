c06805d100540f2b05c36260d87401a3fa7f0355
Parth <parthkiranbhavthankar@gmail.com>
2025-05-19 06:24:16 +0530

    19-05-2025

---
+++ Data Flairr/18-05.py
@@ -0,0 +1,22 @@
+# print('Welcomme in the world of python')
+
+# a,b =int(input('Enter the first number: ')),int(input('Enter the second number: '))
+# print('The sum of the two numbers is:',a+b)
+
+# print('The difference of the two numbers is:',a-b,end='*****')
+
+a=10
+b=15
+c=10
+
+# print(bin(6.5))
+
+# ************************************************** Operators ***************************************************************************
+
+# print(0x2A)
+# num=678.345e-2
+
+# print(num)
+
+a=100
+print(id(a))
