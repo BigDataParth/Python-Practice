0b419840da9420bfb4d2b593321b1b2e9a27e7ba
Parth <parthkiranbhavthankar@gmail.com>
2025-05-20 06:01:57 +0530

    20-5

---
+++ 19-05(string).py
@@ -0,0 +1,41 @@
+# code = "x = 5\ny = 10\nprint(x + y)"
+# exec(code)
+
+# str = "Javats"
+# print(str[len(str)//2])
+
+# n=34
+# print(str(n))
+
+# s = "42"
+# num = int(s)
+# print(num)
+
+# str ='parth'
+# res=[]
+# for i in str:
+#     res.append(i)
+# print(res)
+# print(list(str))
+
+
+# list1 =[1,2,3,4,5]
+# str1=''
+# for i in list1:
+#     str1+=str(i)
+# print(str1)
+
+# s= 'parth'
+# s = s.replace('a','A')
+# print(s)
+
+# a = ['P', 'y', 't', 'h', 'o', 'n']
+# res =''.join(a)
+# print(res)
+
+# tup1 = ('parth', 'python', 'java')
+# res =''.join(tup1)
+# print(res)
+
+# str='hhhhdbdbbbdbbdddwwwwwwxxxxnnnnnn'
+# print(set(str))
