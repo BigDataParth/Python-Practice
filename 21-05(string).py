c7d211788e58cba855dd1afa7a4096880a9ead96
Parth <parthkiranbhavthankar@gmail.com>
2025-05-22 06:00:44 +0530

    22-05-2025

---
+++ 21-05(string).py
@@ -0,0 +1,34 @@
+# s = "Python"
+# print(s.ljust(10, '*'))
+# print(s.rjust(10, '*'))
+# print(s.center(10, '*'))
+# print(s.ljust(10, '*').rjust(20, '#'))
+
+# b = "Adveik"        
+# p = "\u00b2"   # superscript 2
+# print("{}{}".format(b, p))  
+
+# e = "H"        
+# s = "\u2082"   # subscript 2
+# print("{}{}".format(e, s))
+
+# b = '01100111011001010110010101101011'
+# for i in range(0, len(b), 8):
+    
+#     print(chr(int(b[i:i+8], 2)), end='')
+
+# import string
+# a = "The quick  fox jumps over the lazy dog"
+# b= set(string.ascii_lowercase)
+
+# s =set(a.lower())
+
+# res = b<=s
+# print(res)
+
+# s1 = "VISHAKSHI"
+# s2 = "VANSHIKA"
+
+# res = set(s1) & set(s2)
+# print(res)
+
---
+++ Data Flairr/21-05.py
@@ -0,0 +1,45 @@
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(i*j, end="\t")
+#     print()
+
+
+# for i in range(1,10):
+    
+#         print(f'{i} * "{i}"',end="\n")
+
+# n = 5
+
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print('Ram ', end="")
+#     print()
+
+# i=5
+# while i>0:
+#     print('ram')
+
+
+# n =10
+
+# for i in range(1,n+1):
+#     for j in range(n,i-1,-2):
+#         print('* ', end="")
+#     print()
+
+# Transfer Statements
+# for i in range(250):
+#     if i==12:
+#         break
+#     print(i, end=" ")
+# print(f'broken at {i}')
+
+# print('hello \f friends')
+
+# for i in range(1,15):
+#     for j in range(1,11):
+#         print(i*j, end=" ")
+#     print()
+
+for i in range (1,11):
+    print(130*i)
