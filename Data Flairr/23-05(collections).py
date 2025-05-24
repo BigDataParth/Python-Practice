8b73e5101c63dfb1a045d71a74d3868c989d4358
Parth <parthkiranbhavthankar@gmail.com>
2025-05-24 12:57:21 +0530

    24-05-2025

---
+++ Data Flairr/23-05(collections).py
@@ -0,0 +1,10 @@
+lst = ['a',1,2,3,4,5,6,7,8,9]
+# for i in lst:
+#     if isinstance(i, str):
+#         print(i)
+#     else:
+#         print("Not an integer")
+
+# lst.insert(0, 0)
+lst.remove('a')
+print(lst)
