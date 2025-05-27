396188c5f5a783d5ee3385b85411a7b5062a907e
Parth <parthkiranbhavthankar@gmail.com>
2025-05-27 06:33:57 +0530

    27-05

---
+++ Data Flairr/26-05(Arguments).py
@@ -0,0 +1,29 @@
+import module_1 as m1
+import math
+
+# def display():
+#     a=100 #local variable
+#     # print('start with arguments')
+#     print(id(a))
+
+# display()
+# a=200
+# print(id(a))
+
+# print(m1.factorial(5))  # Output: 120 
+# print(m1.reverse(12345))  # Output: 54321
+
+# print(math.factorial(5))  # Output: 120
+# print(math.sqrt(16))  # Output: 4.0
+
+# def addition(a, b ,c=40):
+#     print('Addition:', a + b +c)
+
+# addition(10, 20)  # Output: Addition: 30
+
+# def addition(*std):
+#     for i in std:
+#         print(i)
+
+# addition(10, 20, 30, 40, 50)  # Output: 10 20 30 40 50
+
--- Data Flairr/module_1.py
+++ Data Flairr/module_1.py
@@ -1,5 +1,19 @@
-def display():
-    print('start')
+# def display():
+#     print('start')
 
-print('end')
+# print('end')
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
     
+def reverse(s):
+    n=0
+    while s!=0:
+        r= s%10
+        s = s*10 + r
+        n= n//10
+
+    print(s)
