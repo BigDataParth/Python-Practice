e7b629509f05a655fccf28bce4dfcfd6968d1ed0
Parth <parthkiranbhavthankar@gmail.com>
2025-06-07 06:36:35 +0530

    07/06/2025

---
+++ Data Flairr/06/06.py
@@ -0,0 +1,10 @@
+import array as arr
+
+myarray = arr.array('i',[1,500,250,65])
+# n=int(input('enter the limit of array : '))
+# print("enter elements in array")
+# for i in range(n):
+#     x = int(input())
+#     myarray.append(x)
+
+print(list(reversed(myarray)))
