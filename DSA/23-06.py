a84ad9dbf3a76510d90a9f10b83e4e68dddbec2f
Parth <parthkiranbhavthankar@gmail.com>
2025-06-24 08:02:54 +0530

    24/06

---
+++ DSA/23-06.py
@@ -0,0 +1,30 @@
+# def func(i,n):
+#     if i>n:
+#         return
+#     func(i+1,n)
+#     print(i)
+
+# func(1,10)
+
+
+#factorial of a number using recursion
+# def func(N):
+#     if N==1:
+#         return 1
+#     return N * func(N-1)
+
+# print(func(5))
+
+#CRAZYYYYYYYY
+
+# num =[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# left=0
+# right=len(num)-1
+# while left <= right:
+#     num[left], num[right] = num[right], num[left]
+#     left += 1
+#     right -= 1
+# print(num)
+
+
+    
